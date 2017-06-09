eeea37eab427bb893857e5e981175637ebe7cfd5
Paul D Handy <paul.d.handy@gmail.com>
2017-06-09 05:50:08 -0600

    Revert "updated latestsnapshot to non-static"
    This reverts commit 0b0318f58515d60ce91ab85e849673e1f6ac09f5.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var iri = com.iota.iri;
-var snapshot = IOTA.latestSnapshot;
+//var snapshot = IOTA.latestSnapshot;
+var snapshot = iri.Snapshot.latestSnapshot;
 var Callable = iri.service.CallableRequest;
 var Response = iri.service.dto.IXIResponse;
 
